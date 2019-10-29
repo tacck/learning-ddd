57bd270a23c9c98734d509aa7f904dfc55d8b2ec
Takuya KIHARA <gray@tacck.net>
2019-10-29 10:56:36 +0900

    :card_file_box: Create migration files

---
+++ database/migrations/2019_10_28_064816_create_screenings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScreeningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('screenings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTimeTz('apply_date')->nullable();
+            $table->string('status');
+            $table->string('applicant_email_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('screenings');
+    }
+}
---
+++ database/migrations/2019_10_28_160435_create_interviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInterviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('screening_id');
+            $table->dateTimeTz('screening_date')->nullable();
+            $table->integer('interview_number');
+            $table->string('screening_step_result')->nullable();
+            $table->bigInteger('recruiter_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interviews');
+    }
+}
